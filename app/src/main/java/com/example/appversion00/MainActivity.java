07962570a2c7bbf98ad89805c0debafe223daf4a
Oleg Butaev <oleg.butaew@yandex.ru>
2020-04-05 21:07:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/appversion00/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.appversion00;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Home");
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFargment()).commit();
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFargment();
+                            getSupportActionBar().setTitle("Home");
+                            break;
+                        case R.id.nav_calc:
+                            selectedFragment = new CalcFargment();
+                            getSupportActionBar().setTitle("Calc");
+                            break;
+                        case R.id.nav_news:
+                            selectedFragment = new NewsFargment();
+                            getSupportActionBar().setTitle("News");
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
+}
